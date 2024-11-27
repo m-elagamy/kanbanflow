e1292c3480f4f1106a230345bf8e61a06a8873f4
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-11-27 19:32:02 +0200

    chore: make dark theme is the default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
-          enableSystem
+          defaultTheme="dark"
           disableTransitionOnChange
         >
           <Header />
