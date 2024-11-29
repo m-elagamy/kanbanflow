391b9be5d4b66d84b5dd9caa8dfb8bebb529afd2
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-11-29 15:52:00 +0200

    style: enhance the chosen gradient for the landing title

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -5,16 +5,14 @@ import FloatingParticles from "./floating-particles";
 
 export default function Hero() {
   return (
-    <div className="mx-auto grid h-[calc(100vh-64px)] max-w-3xl place-content-center pb-8 text-center">
+    <div className="grid h-[calc(100vh-64px)] place-content-center pb-8 text-center">
       <div className="relative mb-6">
         <FloatingParticles />
-        <h1 className="text-4xl font-extrabold tracking-tighter md:text-6xl">
-          <span className="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 bg-clip-text text-transparent dark:from-white dark:via-gray-200 dark:to-gray-300">
-            Streamline Your Workflow
-          </span>
+        <h1 className="bg-gradient-to-tr from-[#111] via-[#333] to-[#555] bg-clip-text text-4xl font-extrabold tracking-tighter text-transparent [text-shadow:_0_1px_2px_rgba(0,0,0,0.1)] dark:from-gray-300 dark:via-gray-400 dark:to-gray-200 dark:[text-shadow:_0_1px_2px_rgba(255,255,255,0.1)] md:text-6xl [&::selection]:bg-gray-700/90 [&::selection]:text-white dark:[&::selection]:bg-gray-300/90 dark:[&::selection]:text-gray-900">
+          Streamline Your Workflow
         </h1>
       </div>
-      <p className="mb-8 text-sm text-muted-foreground md:text-base">
+      <p className="mb-8 max-w-3xl text-sm text-muted-foreground md:text-base">
         Take control of your projects and achieve more with ease! KanbanFlow
         helps you manage tasks, organize projects, and boost productivity with
         real-time updates.
