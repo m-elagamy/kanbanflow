168578d227457b5a9cae08964db0d7a805b41b09
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2025-02-17 18:36:24 +0200

    add environment variables to next.config.ts for Clerk keys

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
     reactCompiler: true,
     authInterrupts: true,
   },
+  env: {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  },
 };
 
 export default nextConfig;
