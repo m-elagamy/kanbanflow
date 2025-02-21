fd3b4af37eddde667ebf88c0bc36380abc8f3410
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2025-02-21 18:29:32 +0200

    revert: restore previous layout structure and remove template

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,16 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
+import DashboardSidebar from "@/components/layout/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <SidebarProvider>{children}</SidebarProvider>;
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <SidebarTrigger className="relative -top-11 left-3 z-50 md:top-2" />
+      {children}
+    </SidebarProvider>
+  );
 }
