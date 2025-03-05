3ffb89e79e08a87f4c178f1cdccc3a324b7ab12d
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2025-03-05 15:11:29 +0200

    style: update gradient in FloatingParticles component

--- src/components/landing/floating-particles.tsx
+++ src/components/landing/floating-particles.tsx
@@ -2,30 +2,30 @@
 
 import { motion } from "framer-motion";
 
+const particleVariant = {
+  animate: {
+    scale: [1, 1.2, 1],
+    transition: {
+      repeat: Infinity,
+      duration: 2,
+    },
+  },
+};
+
 const FloatingParticles = () => {
   return (
     <>
       <motion.div
-        className="absolute -left-2 -top-4 h-8 w-8 rounded-full bg-blue-500/10"
-        animate={{
-          scale: [1, 1.2, 1],
-          transition: {
-            repeat: Infinity,
-            duration: 2,
-          },
-        }}
+        className="absolute -left-2 -top-4 h-8 w-8 rounded-full bg-gradient-to-t from-[#51FFC0]/30 via-[#5AD8BC]/30 to-[#74A9BD] dark:from-[#004483]/10 dark:via-[#056F6E]/10 dark:to-[#045943]/10"
+        variants={particleVariant}
+        animate="animate"
         aria-hidden="true"
       />
       <motion.div
-        className="absolute -bottom-4 -right-2 h-8 w-8 rounded-full bg-purple-500/10"
-        animate={{
-          scale: [1, 1.2, 1],
-          transition: {
-            repeat: Infinity,
-            duration: 2,
-            delay: 0.5,
-          },
-        }}
+        className="absolute -bottom-4 -right-2 h-8 w-8 rounded-full bg-gradient-to-t from-[#EEBF2E]/30 via-[#EEA336]/30 to-[#EF7266] dark:from-[#6A0000]/10 dark:via-[#AA2A07]/10 dark:to-[#9A6B12]/10"
+        variants={particleVariant}
+        animate="animate"
+        transition={{ delay: 0.5 }}
         aria-hidden="true"
       />
     </>
