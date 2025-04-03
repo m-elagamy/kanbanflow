d63aa133375ef4f92500ddc1359d48e50d8fbfdc
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2025-04-03 04:26:17 +0200

    refactor(layout): update DashboardLayout to manage sidebar state from cookies

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,17 @@
+import { cookies } from "next/headers";
 import DashboardSidebar from "@/components/layout/sidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={defaultOpen}>
       <DashboardSidebar />
       <SidebarTrigger className="relative -top-11 left-3 z-50 md:top-2" />
       {children}
